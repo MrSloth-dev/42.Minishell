3339b7d22f6401df27572572087249ea45677184
MrSloth <joao8barbosa@gmail.com>
2024-08-31 00:34:32 +0100

    update

--- src/main.c
+++ src/main.c
@@ -3,23 +3,25 @@
 #include <unistd.h>
 
 char	*get_next_line(int fd);
-int	main()
+
+int	main(void)
 {
-	int fd = 0;
-	// int outfile = open("temp", O_CREAT | O_RDWR | O_APPEND);
+	char	*line;
+	int		fd;
+
+	fd = 0;
 	write(2, "\033[2J\033[H", 8);
 	write(STDOUT_FILENO, "Please write your command >>  ", 30);
-	char *line = get_next_line(fd);
-	while(line)
+	line = get_next_line(fd);
+	while (line)
 	{
 		write(STDOUT_FILENO, "Please write your command >>  ", 30);
 		line = get_next_line(fd);
 		if (ft_strlen(line) == 0 || ft_strncmp("exit", line, 5))
 		{
 			free(line);
-			return(0);
+			return (0);
 		}
 	}
-
 	return (0);
 }
