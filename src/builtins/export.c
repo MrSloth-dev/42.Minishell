fe8853f080340438ca739d8a342738b82ab24ede
Joao Pedro Oliveira Moreira Albuquerque Barbosa <joao-pol@c2r6s4.42porto.com>
2024-09-27 15:31:10 +0100

    export is working, now adding plus mode

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -1,27 +1,85 @@
+#include "ft_printf.h"
 #include "minishell.h"
-#include <stdio.h>
-#include <time.h>
 
-void	ft_append_env(char *cmdargs, char **temp, t_shell *shell);
-int		ft_env_exist(char *var, int *j, t_shell *shell);
+void	ft_append_env(char *cmdargs, char **temp);
+int		ft_env_exist(char *var, int *j, char **temp);
 char	*ft_get_var_name(char *env_str);
 
-void	ft_swap_env(char *cmdargs, char **temp, int j, t_shell *shell)
+void	ft_swap_env(char *cmdargs, char **temp, int j)
 {
 	int		k;
 
 	k = 0;
 	j--;
-	while (shell->envp[k])
+	while (temp[k])
 	{
 		if (j == k)
 			temp[k] = ft_strdup(cmdargs);
-		else
-			temp[k] = ft_strdup(shell->envp[k]);
 		if (!temp[k])
 			return ;
 		k++;
 	}
+}
+
+void	ft_join_env(char *cmdargs, char **temp, int j)
+{
+	int		k;
+	int		start;
+
+	start = 0;
+	k = 0;
+	j--;
+	while (temp[k])
+	{
+		if (j == k)
+		{
+			start = ft_strchr(cmdargs[k], '=');
+			temp[k] = ft_strjoin(temp[k], ft_strchr(cmdargs[k], '+')++);
+		}
+		if (!temp[k])
+			return ;
+		k++;
+	}
+}
+
+void	ft_copy_env(char **temp, char **env)
+{
+	int	i;
+
+	i = -1;
+	while (env[++i])
+		temp[i] = ft_strdup(env[i]);
+}
+
+static int	ft_export_duplicate(char **cmdargs, int k)
+{
+	char	*temp;
+
+	temp = cmdargs[k];
+	while (--k)
+		if (cmdargs[k] == temp)
+			return (1);
+	return (0);
+}
+
+static int	ft_export_size_increase(char **cmdargs, t_shell *shell, int *j)
+{
+	int	i;
+	int	k;
+
+	i = 0;
+	k = 1;
+	*j = -1;
+	while (shell->envp[i])
+		i++;
+	while (cmdargs[k])
+	{
+		if (ft_env_exist(cmdargs[k++], j, shell->envp) == -1)
+			i++;
+		if (ft_export_duplicate(cmdargs, k))
+			i--;
+	}
+	return (i);
 }
 
 char	**ft_export(char **cmdargs, t_shell *shell)
@@ -30,39 +88,35 @@ char	**ft_export(char **cmdargs, t_shell *shell)
 	int		i;
 	int		j;
 	int		k;
+	int		plus_mode;
 
-	i = 0;
-	k = 1;
-	j = -1;
-	if (*cmdargs == NULL)
+	if (!cmdargs)
 		return (NULL);
-	while (shell->envp[i])
-		i++;
+	plus_mode = 0;
+	i = ft_export_size_increase(cmdargs, shell, &j);
 	temp = (char **)malloc(sizeof(char *) * (i + 1));
-	while (cmdargs[k])
-		if (ft_env_exist(cmdargs[k++], &j, shell) == -1)
-			i++;
+	ft_copy_env(temp, shell->envp);
 	k = 0;
 	while (cmdargs[++k])
 	{
 		j = -1;
-		if (ft_env_exist(cmdargs[k], &j, shell) != -1)
-			ft_swap_env(cmdargs[k], temp, j, shell);
+		if ((ft_strchr(cmdargs[k], '=') - ft_strchr(cmdargs[k], '+')) == 1)
+			plus_mode = 1;
+		if (ft_env_exist(cmdargs[k], &j, temp) != -1 && plus_mode)
+			ft_swap_env(cmdargs[k], temp, j);
+		else if (ft_env_exist(cmdargs[k], &j, temp) != -1 && !plus_mode)
+			ft_swap_env(cmdargs[k], temp, j);
 		else
-			ft_append_env(cmdargs[k], temp, shell);
+			ft_append_env(cmdargs[k], temp);
 	}
-	i = 0;
-	while (temp[i])
-		ft_printf(1, "%s\n", temp[i++]);
 	return (temp);
 }
 
-void	ft_append_env(char *cmdargs, char **temp, t_shell *shell)
+void	ft_append_env(char *cmdargs, char **temp)
 {
 	int		i;
 
 	i = 0;
-	(void)shell;
 	while (temp[i])
 		i++;
 	temp[i++] = ft_strdup(cmdargs);
@@ -81,7 +135,7 @@ char	*ft_get_var_name(char *env_str)
 	return (ft_substr(env_str, 0, i));
 }
 
-int	ft_env_exist(char *var, int *j, t_shell *shell)
+int	ft_env_exist(char *var, int *j, char **temp)
 {
 	char	*name;
 	int		len;
@@ -89,9 +143,9 @@ int	ft_env_exist(char *var, int *j, t_shell *shell)
 	*j = 0;
 	len = ft_strlen(var);
 	var = ft_get_var_name(var);
-	while (shell->envp[*j])
+	while (temp[*j])
 	{
-		name = ft_get_var_name(shell->envp[(*j)++]);
+		name = ft_get_var_name(temp[(*j)++]);
 		if (ft_strncmp(var, name, len) == 0)
 			return (free(name), *j);
 		free(name);
@@ -99,7 +153,7 @@ int	ft_env_exist(char *var, int *j, t_shell *shell)
 	return (-1);
 }
 
-/* char	*ft_strjoin_free(char const *s1, char const *s2)
+char	*ft_strjoin_free(char const *s1, char const *s2)
 {
 	char	*join;
 	int		i;
@@ -126,4 +180,4 @@ int	ft_env_exist(char *var, int *j, t_shell *shell)
 	free((void *)s1);
 	free((void *)s2);
 	return (join);
-} */
+}
