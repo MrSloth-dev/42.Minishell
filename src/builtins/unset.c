f92564f6432a334e6aa92eafbca816b94dc6dc69
isilva-t <ivanrfteixeira@gmail.com>
2024-10-02 09:57:31 +0100

    update unset issues

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -1,8 +1,5 @@
 #include "minishell.h"
 
-static int		ft_env_exist(char *var, int *j, t_shell *shell);
-static char	*ft_get_var_name(char *env_str);
-
 static void	ft_remove_env(char **temp, int j, t_shell *shell)
 {
 	int		k;
@@ -50,34 +47,4 @@ char	**ft_unset(char **cmdargs, t_shell *shell)
 	while (temp[i])
 		printf("%s\n", temp[i++]);
 	return (temp);
-}
-
-static char	*ft_get_var_name(char *env_str)
-{
-	int	i;
-
-	i = 0;
-	while (env_str[i] && env_str[i] != '=')
-		i++;
-	if (i == (int)ft_strlen(env_str))
-		return (NULL);
-	return (ft_substr(env_str, 0, i));
-}
-
-static int	ft_env_exist(char *var, int *j, t_shell *shell)
-{
-	char	*name;
-	int		len;
-
-	*j = 0;
-	len = ft_strlen(var);
-	var = ft_get_var_name(var);
-	while (shell->envp[*j])
-	{
-		name = ft_get_var_name(shell->envp[(*j)++]);
-		if (ft_strncmp(var, name, len) == 0)
-			return (free(name), *j);
-		free(name);
-	}
-	return (-1);
 }
