0073552c1f4ff0e10c20fa9b677b456950ab04e3
isilva-t <ivanrfteixeira@gmail.com>
2024-10-16 08:49:55 +0100

    ft_isbuiltin - change "if/else if" to same order as "Subject"

--- src/main.c
+++ src/main.c
@@ -3,19 +3,19 @@
 
 int	ft_isbuiltin(char *content)
 {
-	if (!ft_strcmp(content, "exit"))
+	if (!ft_strcmp(content, "echo"))
 		return (1);
-	else if (!ft_strcmp(content, "env"))
+	else if (!ft_strcmp(content, "cd"))
 		return (1);
 	else if (!ft_strcmp(content, "pwd"))
 		return (1);
 	else if (!ft_strcmp(content, "export"))
 		return (1);
-	else if (!ft_strcmp(content, "echo"))
-		return (1);
 	else if (!ft_strcmp(content, "unset"))
 		return (1);
-	else if (!ft_strcmp(content, "cd"))
+	else if (!ft_strcmp(content, "env"))
+		return (1);
+	else if (!ft_strcmp(content, "exit"))
 		return (1);
 	else
 		return (0);
