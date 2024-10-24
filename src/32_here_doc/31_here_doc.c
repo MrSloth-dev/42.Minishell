4e8f00e8f8c9303cbd7d86fd23e512aef20dd4e2
isilva-t <ivanrfteixeira@gmail.com>
2024-10-24 16:24:52 +0100

    heredoc before change itselft

--- src/32_here_doc/31_here_doc.c
+++ src/32_here_doc/31_here_doc.c
@@ -4,57 +4,7 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 
-// static void	ft_handle_sig_here(int signal)
-// {
-// 	if (signal == SIGINT)
-// 	{
-//
-// 		printf("\n");
-// 		rl_replace_line("", 0);
-// 		rl_on_new_line();
-// 		rl_redisplay();
-// 	}
-// }
-//
-// static void	ft_start_sig_in_this_scope(void)
-// {
-// 	struct sigaction	sa;
-//
-// 	sa.sa_handler = &ft_handle_sig_here;
-// 	sa.sa_flags = SA_RESTART;
-// 	sigemptyset(&sa.sa_mask);
-// 	sigaddset(&sa.sa_mask, SIGINT);
-// 	sigaction(SIGINT, &sa, NULL);
-// 	signal(SIGQUIT, SIG_IGN);
-// }
-//
-void	ft_here_doc(t_shell *sh, char *delimiter, int hd_id, char *file)
-{
-	char	*line;
 
-	sh->heredoc_fd[hd_id] = open(file, O_RDWR | O_APPEND, 0644);
-	sh->exit_status = EXIT_SUCCESS;
-	line = NULL;
-	while (1)
-	{
-		line = readline("> ");
-		if (line == NULL)
-		{
-			close(sh->heredoc_fd[hd_id]);
-			break ;
-		}
-		else if (ft_strcmp(line, delimiter)  == 0)
-		{
-			close(sh->heredoc_fd[hd_id]);
-			break ;
-		}
-		else if ((line && *(line)))
-		{
-			ft_printf(sh->heredoc_fd[hd_id], "%s", line);
-			write(sh->heredoc_fd[hd_id], "\n", 1);
-		}
-	}
-}
 void	ft_sig_default(void)
 {
 	signal(SIGINT, SIG_DFL);
@@ -72,11 +22,42 @@ void	ft_sig_ignore(void)
 	signal(SIGQUIT, SIG_IGN);
 }
 
+void	ft_here_doc(t_shell *sh, char *delimiter, int hd_id, char *file)
+{
+	char	*line;
+
+	sh->heredoc_fd[hd_id] = open(file, O_RDWR | O_APPEND, 0644);
+	sh->exit_status = EXIT_SUCCESS;
+	line = NULL;
+	while (1)
+	{
+		ft_sig_heredoc();
+		line = readline("> ");
+		if (line == NULL)
+		{
+			ft_printf(1, "%s: warning: here-document delimited by end-of-file (wanted `%s')\n", sh->prog_name, delimiter);
+			close(sh->heredoc_fd[hd_id]);
+			break ;
+		}
+		else if (ft_strcmp(line, delimiter)  == 0)
+		{
+			close(sh->heredoc_fd[hd_id]);
+			break ;
+		}
+		else if ((line && *(line)))
+		{
+			ft_printf(sh->heredoc_fd[hd_id], "%s", line);
+			write(sh->heredoc_fd[hd_id], "\n", 1);
+		}
+		line = ft_free(line);
+	}
+}
+
 void	ft_run_heredocs(t_token *token, t_shell *sh)
 {
 	t_iter	s;
 	int		pid;
-//	int		status;
+	int		status;
 
 	if (!token || !sh)
 		return ;
@@ -86,7 +67,7 @@ void	ft_run_heredocs(t_token *token, t_shell *sh)
 	// ft_start_sig_in_this_scope();
 	if (pid == 0)
 	{
-//		ft_sig_heredoc();
+		ft_sig_default();
 		while (s.cur)
 		{		
 			if (s.cur->file)
@@ -99,16 +80,15 @@ void	ft_run_heredocs(t_token *token, t_shell *sh)
 	}
 	else
 	{
-		//wait(0);
-		waitpid(pid, NULL, 0);
-//		ft_sig_ignore();
-	//	waitpid(0, &status, 0);
-//		ft_sig_default();
-		// if (WIFSIGNALED(status) && WTERMSIG(status) == SIGINT)
-		// {
-		// 	ft_printf(1, "\n");
-		// 	sh->head = NULL;
-		// }
+		//waitpid(pid, NULL, 0);
+		ft_sig_ignore();
+		waitpid(pid, &status, 0);
+		if (WIFSIGNALED(status) && WTERMSIG(status) == SIGINT)
+		{
+			ft_printf(1, "\n");
+			sh->head = NULL;
+		}
+		ft_sig_default();
 	}
 }
 
